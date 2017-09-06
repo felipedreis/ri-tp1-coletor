93f5a5e823b8973f28ed15f8d0128744571f312d
Vinicius <viniciusvmda@gmail.com>
2017-09-06 12:38:03 -0300

    Adicionadas alteracoes da ultima aula

--- ColetorCodingDojo/src/crawler/escalonadorCurtoPrazo/EscalonadorSimples.java
+++ ColetorCodingDojo/src/crawler/escalonadorCurtoPrazo/EscalonadorSimples.java
@@ -7,6 +7,8 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -27,39 +29,82 @@ public class EscalonadorSimples implements Escalonador{
 	private Map<Servidor, List<URLAddress>> fila;
 	private Set<URLAddress> pagVisitadas;
 	private Queue<Servidor> servidores;
+	private Map<String, Record> mapaRobots;
+	private int cont_paginas;
+	private final int limite_paginas = 10;
 	
 	
 	public EscalonadorSimples(){
-		fila = new HashMap<>();
+		fila = new LinkedHashMap<>();
 		pagVisitadas = new HashSet<>();
 		servidores = new PriorityQueue<>();
+		mapaRobots = new HashMap<>();
+		cont_paginas = 0;
 	}
 	
 	
 	@Override
 	public synchronized URLAddress getURL() {
+		boolean isAcessivel = true;
 		do{
-			Servidor servidor = servidores.remove();
-			if(servidor.isAccessible()){
-				servidores.add(servidor);
-				try{
-					wait(1000);
-				}catch(InterruptedException e){
-					e.printStackTrace();
+			if(!servidores.isEmpty()){
+				for (Servidor servidor : servidores){
+					if(!servidor.isAccessible()){
+						isAcessivel = false;
+					}else{
+						if(!fila.get(servidor).isEmpty()){
+							URLAddress url = fila.get(servidor).remove(0);
+							servidor.acessadoAgora();
+							this.notifyAll();
+							servidores.add(servidor);
+							return url;
+						}else{
+							servidores.remove(servidor);
+						}
+					}
 				}
-				continue;
-			}else{
-				if(!fila.get(servidor).isEmpty()){
-					URLAddress url = fila.get(servidor).remove(0);
-					servidor.acessadoAgora();
-					servidores.add(servidor);
-					return url;
-				}else{
-					servidores.add(servidor);
+				if(!isAcessivel){
+					try{
+						this.wait(1000);
+					}catch(InterruptedException e){
+						e.printStackTrace();
+					}
 				}
 			}
-		}while(true);
+		} while(!this.finalizouColeta());
+		return null;
 	}
+	
+	/*
+	@Override
+	public synchronized URLAddress getURL() {
+		do{
+			if(!servidores.isEmpty()){
+				Servidor servidor = servidores.remove();
+				if(!servidor.isAccessible()){
+					servidores.add(servidor);
+					try{
+						this.wait(1000);
+					}catch(InterruptedException e){
+						e.printStackTrace();
+					}
+					continue;
+				}else{
+					if(!fila.get(servidor).isEmpty()){
+						URLAddress url = fila.get(servidor).remove(0);
+						servidor.acessadoAgora();
+						this.notifyAll();
+						servidores.add(servidor);
+						return url;
+					}else{
+						servidores.add(servidor);
+					}
+				}
+			}
+		} while(!this.finalizouColeta());
+		return null;
+	}
+	 * */
 
 	@Override
 	public synchronized boolean adicionaNovaPagina(URLAddress urlAdd) {
@@ -84,26 +129,30 @@ public class EscalonadorSimples implements Escalonador{
 
 
 	@Override
-	public Record getRecordAllowRobots(URLAddress url) {
-		// TODO Auto-generated method stub
+	public synchronized Record getRecordAllowRobots(URLAddress url) {
+		if (mapaRobots.containsKey(url.getDomain())) {
+			return mapaRobots.get(url.getDomain());
+		}
 		return null;
 	}
 
 	@Override
-	public void putRecorded(String domain, Record domainRec) {
-		// TODO Auto-generated method stub
-		
+	public synchronized void putRecorded(String domain, Record domainRec) {
+		if (!mapaRobots.containsKey(domain)) {
+			mapaRobots.put(domain, domainRec);
+		}
 	}
+	
 	@Override
-	public boolean finalizouColeta() {
-		// TODO Auto-generated method stub
+	public synchronized boolean finalizouColeta() {
+		if (cont_paginas >= limite_paginas)
+			return true;
 		return false;
 	}
 
 	@Override
-	public void countFetchedPage() {
-		// TODO Auto-generated method stub
-		
+	public synchronized void countFetchedPage() {
+		cont_paginas++;
 	}
 
 	
